1449ecbe365134f327bff5be4506b3170780c632
mditbbte <andrii.ds.kor@gmail.com>
2019-12-09 01:42:08 +0200

    Updates

---
+++ precache-manifest.1e2a2459f64124ce233f5ece2a0f94d4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "07f576c8423441ab4821e5cb43f084f9",
+    "url": "/goit-react-hw-03-components/index.html"
+  },
+  {
+    "revision": "f45afd8e6274ed72f24b",
+    "url": "/goit-react-hw-03-components/static/css/main.e34ab982.chunk.css"
+  },
+  {
+    "revision": "13c1e3ae52b2cb4ff947",
+    "url": "/goit-react-hw-03-components/static/js/2.e1e9eb9c.chunk.js"
+  },
+  {
+    "revision": "f45afd8e6274ed72f24b",
+    "url": "/goit-react-hw-03-components/static/js/main.72e46e92.chunk.js"
+  },
+  {
+    "revision": "c660f03251d9b92990da",
+    "url": "/goit-react-hw-03-components/static/js/runtime-main.04411322.js"
+  }
+]);
